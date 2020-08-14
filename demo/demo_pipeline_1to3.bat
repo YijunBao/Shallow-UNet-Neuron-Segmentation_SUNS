260d436a12cbc97aa06f5988dc7a0c31b96aae39
YijunBao <yijun.bao@duke.edu>
2020-08-14 14:11:06 -0400

    Update demo_pipeline_1to3.bat

--- demo/demo_pipeline_1to3.bat
+++ demo/demo_pipeline_1to3.bat
@@ -1,12 +1,12 @@
-REM REM Prepare for FFT-based spatial filtering
-REM python demo_learn_wisdom.py
-REM python demo_learn_wisdom_2d.py
+REM Prepare for FFT-based spatial filtering
+python demo_learn_wisdom.py
+python demo_learn_wisdom_2d.py
 
-REM REM Training pipeline
-REM python demo_train_CNN_params_1to3.py
+REM Training pipeline
+python demo_train_CNN_params_1to3.py
 
-REM REM Run SUNS batch
-REM python demo_test_batch_1to3.py
+REM Run SUNS batch
+python demo_test_batch_1to3.py
 REM Run SUNS online
 python demo_test_online_1to3.py
 REM Run SUNS online with tracking
