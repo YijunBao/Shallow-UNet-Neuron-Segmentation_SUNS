e40eaebba2591754a509697501ef4405f8f19163
YijunBao <yijun.bao@duke.edu>
2020-09-22 15:13:51 -0400

    Compress the output masks for demo

--- paper reproduction/ABO/pipeline_ABO_online.bat
+++ paper reproduction/ABO/pipeline_ABO_online.bat
@@ -1,5 +1,9 @@
 python "C:\Matlab Files\timer\timer_start_next_2.py"
 
+python test_online_ABO_noSF.py
+REM python test_online_ABO_complete.py
+REM python test_online_track_ABO_noSF.py
+REM python test_online_track_ABO_complete.py
 python test_online_ABO_noSF_vary_merge.py 10
 python test_online_ABO_noSF_vary_merge.py 20
 REM python test_online_ABO_noSF_vary_merge.py 30
