721a3c927cbcfe4a7ce9e8e0fc7382f443df93d9
YijunBao <yijun.bao@duke.edu>
2021-02-05 13:10:09 -0500

    Update test_batch_noSF_CM.py

--- paper reproduction/CaImAn dataset/test_batch_noSF_CM.py
+++ paper reproduction/CaImAn dataset/test_batch_noSF_CM.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     useSF=False # True if spatial filtering is used in pre-processing.
     useTF=True # True if temporal filtering is used in pre-processing.
     useSNR=True # True if pixel-by-pixel SNR normalization filtering is used in pre-processing.
-    med_subtract=True # True if the spatial median of every frame is subtracted before temporal filtering.
+    med_subtract=False # True if the spatial median of every frame is subtracted before temporal filtering.
         # Can only be used when spatial filtering is not used. 
     prealloc=True # True if pre-allocate memory space for large variables in pre-processing. 
             # Achieve faster speed at the cost of higher memory occupation.
@@ -50,9 +50,9 @@ if __name__ == '__main__':
         # folder of the ".mat" files stroing the GT masks in sparse 2D matrices
         dir_GTMasks = dir_video + 'GT Masks\\FinalMasks_' 
 
-        dir_parent = dir_video + 'noSF_subtract\\' # folder to save all the processed data
-        dir_sub = '1skip\\'
-        dir_output = dir_parent + dir_sub + 'output_masks\\' # folder to save the segmented masks and the performance scores
+        dir_parent = dir_video + 'noSF\\' # folder to save all the processed data
+        dir_sub = ''
+        dir_output = dir_parent + dir_sub + 'output_masks new\\' # folder to save the segmented masks and the performance scores
         dir_params = dir_parent + dir_sub + 'output_masks\\' # folder of the optimized hyper-parameters
         weights_path = dir_parent + dir_sub + 'Weights\\' # folder of the trained CNN
         if not os.path.exists(dir_output):
@@ -113,8 +113,8 @@ if __name__ == '__main__':
                 'cons':Params_post_mat['cons'][0][0,0]}
 
             # The entire process of SUNS batch
-            Masks, Masks_2, time_total, time_frame = suns_batch(
-                dir_video, Exp_ID, filename_CNN, Params_pre, Params_post, dims, batch_size_eval, \
+            Masks, Masks_2, times_active, time_total, time_frame = suns_batch(
+                dir_video, Exp_ID, filename_CNN, Params_pre, Params_post, batch_size_eval, \
                 useSF=useSF, useTF=useTF, useSNR=useSNR, med_subtract=med_subtract, \
                 useWT=useWT, prealloc=prealloc, display=display, p=p)
 
